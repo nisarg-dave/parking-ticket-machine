1cbd94418f67f0778fb3f8dbc20140fe7af384a0
Nisarg Dave <ndave630@gmail.com>
2023-05-03 21:10:16 +0800

    Able to calculate time difference and cost

--- src/com/parking/ticket/TicketMachine.java
+++ src/com/parking/ticket/TicketMachine.java
@@ -1,10 +1,10 @@
 package com.parking.ticket;
 
 import java.time.format.DateTimeFormatter;
-import java.util.Locale;
 import java.util.Scanner;
 import java.time.LocalDate;
 import java.time.LocalTime;
+import java.time.Duration;
 
 public class TicketMachine {
 
@@ -13,56 +13,64 @@ public class TicketMachine {
     private DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern("HH:mm");
 
 
-    public static void main (String args[]){
+    public static void main(String args[]) {
 
         TicketMachine ticketMachine = new TicketMachine();
 
         // initialise Scanner
         Scanner sc = new Scanner(System.in);
 
-        while(true){
+        while (true) {
             ticketMachine.printUserMenu(sc);
         }
     }
 
-    public void printUserMenu(Scanner sc){
+    public void printUserMenu(Scanner sc) {
         int carParkSelection;
 
-       do {
-          System.out.println("\nPlease select which car park you would like to go to.");
-          System.out.println("1) City Car Park");
-          System.out.println("2) Foreshore Park");
-          System.out.println("3) Hotel Car Park");
-          System.out.println("4) Underground Car Park");
-          System.out.println("5) Western Car Park");
-          System.out.println("6) Quit");
-          System.out.print("Enter choice: ");
-          carParkSelection = sc.nextInt();
+        do {
+            System.out.println("\nPlease select which car park you would like to go to.");
+            System.out.println("1) City Car Park");
+            System.out.println("2) Foreshore Park");
+            System.out.println("3) Hotel Car Park");
+            System.out.println("4) Underground Car Park");
+            System.out.println("5) Western Car Park");
+            System.out.println("6) Quit");
+            System.out.print("Enter choice: ");
+            carParkSelection = sc.nextInt();
 
-          if (carParkSelection < 1 || carParkSelection > 6){
-              System.out.println("Invalid choice. Please choose 1-6");
-          }
-      } while(carParkSelection < 1 || carParkSelection > 6);
+            if (carParkSelection < 1 || carParkSelection > 6) {
+                System.out.println("Invalid choice. Please choose 1-6");
+            }
+        } while (carParkSelection < 1 || carParkSelection > 6);
 
-        switch(carParkSelection){
+        switch (carParkSelection) {
             case 1:
                 cityCarParkMenu(sc);
         }
 
-        if(carParkSelection != 6){
+        if (carParkSelection != 6) {
             printUserMenu(sc);
         }
     }
 
-    public double calculateCost(LocalTime entranceTime, LocalTime exitTime, double hourlyCost){
-        double hourDiff = exitTime.getHour() - entranceTime.getHour();
-        double minuteDiff = exitTime.getMinute() - entranceTime.getMinute();
-        double cost = (hourlyCost * hourDiff) + ((minuteDiff/60) * hourlyCost);
-        System.out.println(cost);
-        return 0;
+    public double calculateCost(LocalTime entranceTime, LocalTime exitTime, double hourlyCost) {
+        double cost;
+        Duration timeElapsed = Duration.between(entranceTime, exitTime);
+        long timeElapsedInMinutes = timeElapsed.toMinutes();
+
+        if (timeElapsedInMinutes <= 30) {
+            cost = 0;
+        } else if (entranceTime.isAfter(LocalTime.parse("21:30"))) {
+            cost = 0;
+        } else {
+            cost = ((double) timeElapsedInMinutes / 60) * hourlyCost;
+        }
+
+        return cost;
     }
 
-    public void cityCarParkMenu(Scanner sc){
+    public void cityCarParkMenu(Scanner sc) {
         // User selection
         int userSelection;
 
@@ -74,11 +82,12 @@ public class TicketMachine {
         // Existing Ticket inits
         String ticketNumberOfUser;
         Ticket ticketOfUser;
+        LocalTime exitTime;
         double cost;
 
-        do{
+        do {
             System.out.printf("\nWelcome to City Car Park. Bays available: %d.", this.cityCarPark.getBaysAvailable());
-            if(this.cityCarPark.getBaysAvailable() == 0){
+            if (this.cityCarPark.getBaysAvailable() == 0) {
                 System.out.println("\nSorry, this car park has no bays available. Please go to another one.");
                 printUserMenu(sc);
             }
@@ -88,13 +97,13 @@ public class TicketMachine {
             System.out.println("3) Quit");
             System.out.print("Enter choice: ");
             userSelection = sc.nextInt();
-            if (userSelection < 1 || userSelection > 3){
+            if (userSelection < 1 || userSelection > 3) {
                 System.out.println("Invalid choice. Please choose 1-3");
             }
-        } while(userSelection < 1 || userSelection > 3);
+        } while (userSelection < 1 || userSelection > 3);
 
 
-        switch(userSelection){
+        switch (userSelection) {
             case 1:
                 Ticket t = new Ticket();
                 this.cityCarPark.bayTaken(t);
@@ -111,22 +120,24 @@ public class TicketMachine {
                 System.out.print("\nPlease enter your ticket number: ");
                 ticketNumberOfUser = sc.next();
                 ticketOfUser = this.cityCarPark.getTicketOfUser(ticketNumberOfUser);
-                if(ticketOfUser != null){
+                if (ticketOfUser != null) {
                     ticketOfUser.setExitTime();
-//                    cost = calculateCost(ticketOfUser.getEntranceTime(),ticketOfUser.getExitTime(), this.cityCarPark.getHourlyCost());
-                    cost = calculateCost(LocalTime.parse("07:47"),LocalTime.parse("16:08"), this.cityCarPark.getHourlyCost());
-
-                }
-                else{
+                    exitTime = ticketOfUser.getExitTime();
+                    cost = calculateCost(ticketOfUser.getEntranceTime(), ticketOfUser.getExitTime(), this.cityCarPark.getHourlyCost());
+//                    cost = calculateCost(LocalTime.parse("07:47"), LocalTime.parse("16:04"), this.cityCarPark.getHourlyCost());
+//                    cost = calculateCost(LocalTime.parse("21:45"), LocalTime.parse("23:04"), this.cityCarPark.getHourlyCost());
+                    System.out.println("\n### Printing Receipt ###");
+                    System.out.printf("Exit Time: %s\n", this.timeFormatter.format(exitTime));
+                    System.out.printf("Cost: $%.2f\n", cost);
+                } else {
                     System.out.println("Cannot find ticket number. Please try again.");
                 }
-            }
+        }
 
-        if(userSelection != 3){
+        if (userSelection != 3) {
             cityCarParkMenu(sc);
-        };
+        }
     }
-
 
 
 }
